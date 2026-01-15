fadc1e61f97a2334c7b51c61ea73ece3191387cf
kkk <carck1dev@proton.me>
2026-01-15 12:44:46 +0200

    front: fix api url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080/api'
+const API_BASE_URL = '/api'
 
 // Создаем экземпляр axios
 const apiClient = axios.create({
@@ -23,22 +23,22 @@ apiClient.interceptors.response.use(
 export const blogApi = {
     // Получить все посты
     getBlogs: (params = {}) => apiClient.get('/blog/', { params }),
-    
+
     // Получить один пост
     getBlog: (id) => apiClient.get(`/blog/${id}/`),
-    
+
     // Создать новый пост
     createBlog: (data) => apiClient.post('/blog/', data),
-    
+
     // Обновить пост
     updateBlog: (id, data) => apiClient.put(`/blog/${id}/`, data),
-    
+
     // Частично обновить пост
     patchBlog: (id, data) => apiClient.patch(`/blog/${id}/`, data),
-    
+
     // Удалить пост
     deleteBlog: (id) => apiClient.delete(`/blog/${id}/`),
-    
+
     // Пинг для проверки соединения
     ping: () => apiClient.get('/ping/'),
     }
